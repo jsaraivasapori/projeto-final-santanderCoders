863715ec822452b410e822118208818920bd2421
João <joaosaraivasapori@gmail.com>
2024-05-13 14:38:42 -0300

    Criação da rota vazia com redirecionamento para appointments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './modules/auth/auth.component';
+import { RegisterComponent } from './modules/auth/components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'appointments'
+    },
+
+   
+];
