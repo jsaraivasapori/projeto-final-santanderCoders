ad34ba9ae14ba56eac2a775837c9e6b08e6a15eb
João <joaosaraivasapori@gmail.com>
2024-05-13 14:32:51 -0300

    Criação do componente  medical-appointments

---
+++ src/app/modules/medical-appointments/medical-appointments.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MedicalAppointmentsComponent } from './medical-appointments.component';
+
+describe('MedicalAppointmentsComponent', () => {
+  let component: MedicalAppointmentsComponent;
+  let fixture: ComponentFixture<MedicalAppointmentsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MedicalAppointmentsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MedicalAppointmentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/medical-appointments/medical-appointments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medical-appointments',
+  standalone: true,
+  imports: [],
+  templateUrl: './medical-appointments.component.html',
+  styleUrl: './medical-appointments.component.scss'
+})
+export class MedicalAppointmentsComponent {
+
+}
