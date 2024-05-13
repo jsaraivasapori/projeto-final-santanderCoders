4b66e3c1e309a58f8ef6a4380b196747957d95fd
João <joaosaraivasapori@gmail.com>
2024-05-13 14:39:49 -0300

    Criação da rota auth com suas rotas filhas register e login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,27 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './modules/auth/auth.component';
 import { RegisterComponent } from './modules/auth/components/register/register.component';
+import { LoginComponent } from './modules/auth/components/login/login.component';
 
 export const routes: Routes = [
     {
         path: '',
         redirectTo: 'appointments'
     },
+    {
+        path: 'auth',
+        component: AuthComponent,
+        children :[
+            {
+                path: 'register',
+                component: RegisterComponent
+            },
 
-   
+            {
+                path: 'login',
+                component: LoginComponent
+            }
+        ]
+
+    }
 ];
