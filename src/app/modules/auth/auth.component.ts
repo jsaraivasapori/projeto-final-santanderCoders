452d5f94eb721846bd19819d9a3fe947693ebd0c
João Vitor <joaosaraivasapori@gmail.com>
2024-05-16 20:36:53 -0300

    update - ssr activated

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
