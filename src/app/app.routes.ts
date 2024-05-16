82fba9efdf2d9a79a42528e25849abde295bc8e9
Lucas Pena Araújo dos Santos <lucaspsantos003@gmail.com>
2024-05-15 21:56:18 -0300

    Alteração do css do appComponent, + pasta core + pasta dashboard + rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,32 +2,42 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './modules/auth/auth.component';
 import { RegisterComponent } from './modules/auth/components/register/register.component';
 import { LoginComponent } from './modules/auth/components/login/login.component';
-import { MedicalAppointmentsComponent } from './modules/medical-appointments/medical-appointments.component';
+import { UserdashboardComponent } from './modules/dashboards/components/userdashboard/userdashboard.component';
+import { AdmindashboardComponent } from './modules/dashboards/components/admindashboard/admindashboard.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'appointments'
-    },
-    {
-        path: 'auth',
-        component: AuthComponent,
-        children :[
-            {
-                path: 'register',
-                component: RegisterComponent
-            },
+  {
+    path: '',
+    redirectTo: 'appointments',
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
 
-            {
-                path: 'login',
-                component: LoginComponent
-            }
-        ]
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ],
+  },
 
-    },
+  {
+    path: 'appointments',
+    children: [
+      {
+        path: 'admin',
+        component:AdmindashboardComponent ,
+      },
 
-    {
-        path: 'appointments',
-        component: MedicalAppointmentsComponent
-    }
+      {
+        path: 'user',
+        component: UserdashboardComponent,
+      },
+    ],
+  },
 ];
---
+++ src/app/modules/dashboards/components/userdashboard/userdashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userdashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './userdashboard.component.html',
+  styleUrl: './userdashboard.component.scss'
+})
+export class UserdashboardComponent {
+
+}
