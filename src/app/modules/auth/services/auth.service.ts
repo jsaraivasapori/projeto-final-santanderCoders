3ebd465ff948f772b9106120229ae0292ba64034
João Vitor <joaosaraivasapori@gmail.com>
2024-05-17 01:12:46 -0300

    update

---
+++ src/app/modules/auth/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserCredentials } from '../models/user-credentials.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  apiUrl = `${environment.API_URL}/auth`
+ 
+
+  constructor(private http: HttpClient) {}
+
+  login(payload: UserCredentials): Observable<AuthenticatedUser> {
+    return this.http.post<AuthenticatedUser>(`${this.apiUrl}/login`, payload);
+  }
+}
