30140a59d26c0ac4bb53dc4a23e267ff6b6dc5d2
João Vitor <joaosaraivasapori@gmail.com>
2024-05-16 22:42:26 -0300

    Criação da tela de login

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatCardModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
