654e7fa59f1cbf23350c6f8bf90573643440fd29
Lucas Pena Araújo dos Santos <lucaspsantos003@gmail.com>
2024-05-17 07:53:35 -0300

    rotas + enviroments + add httpclient + dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './modules/auth/auth.component';
+import { RegisterComponent } from './modules/auth/components/register/register.component';
+import { LoginComponent } from './modules/auth/components/login/login.component';
+import { UserdashboardComponent } from './modules/dashboards/components/userdashboard/userdashboard.component';
+import { AdmindashboardComponent } from './modules/dashboards/components/admindashboard/admindashboard.component';
+import { DashboardComponent } from './modules/dashboards/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'appointments',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ],
+  },
+
+  {
+    path: 'appointments',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'admin',
+        component: AdmindashboardComponent,
+      },
+
+      {
+        path: 'user',
+        component: UserdashboardComponent,
+      },
+    ],
+  },
+];
