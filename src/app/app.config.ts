3369671dc8c74523ba1206a707b49cf737e27cd8
João Vitor <joaosaraivasapori@gmail.com>
2024-05-20 16:30:43 -0300

    Alguns ajustes no tamnho do form de agendamento

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(), 
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([authInterceptor])), 
-    provideAnimationsAsync()]
+    provideAnimationsAsync(),
+  ]
 };
